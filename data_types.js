6b114b1d83b2375619778cf9e8ccd03fadb903ce
abhay-raj24 <websbase90@gmail.com>
2024-07-15 12:11:22 +0530

    Add new parah

---
+++ data_types.js
@@ -0,0 +1,21 @@
+//alert(10 + 10) we are using nodejs / not browser//
+//Code Readability shoud be high//
+
+let name = "Rajan"
+let age = 20
+let isloggedIn = false 
+
+/* Number = 2 power 53
+bigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+object
+*/
+
+//typeof null => object
+//typeof undefined => undefined
+
+console.log(typeof age)
---
+++ variables.js
@@ -0,0 +1,19 @@
+const accountId = 20537
+let accountEmail = "abhi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Uttarakhand"
+
+//accountId = 2//
+accountEmail = "sdags@gmail.com"
+accountPassword = "67899"
+accountCity = "Noida Sec 16"
+let accountState
+
+
+console.log("Both Methods:")
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState)
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
