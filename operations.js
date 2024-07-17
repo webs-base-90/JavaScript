b1a2604f4faa33e7cb9591f3eec2412aea80944e
abhay-raj24 <websbase90@gmail.com>
2024-07-17 20:23:30 +0530

    operations of datatypes

---
+++ operations.js
@@ -0,0 +1,16 @@
+//let value = 3
+//let negValue = -value
+//console.log(negValue)
+
+//let str1= "Abhay"
+//let str2= " Rajan"
+//let str3= str1 + str2
+//console.log(str3)
+
+// console.log("1" + 2 + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "2")
+
+let gameCounter = 100
+gameCounter--;
+console.log(gameCounter)
