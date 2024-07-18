39214ecbfb3730ecf3701ee7b3c9d5ad18bbd892
abhay-raj24 <websbase90@gmail.com>
2024-07-18 17:57:54 +0530

    datatype of summery notes

---
+++ datatypes-summery.js
@@ -0,0 +1,25 @@
+
+
+// primitive => String,Number,Boolean,null,undefined,Symbol, BigInt.
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(anotherId == id)
+
+// const bigNumber = 25634723477398n
+
+// non-primitive/refrence => Array,Object, Function.
+
+// const heros = ["Ironman","saitama","satoru"];
+
+// let myObj = {
+//     name: "Abhay",
+//     age: 18,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World");
+// }
+
+// console.log(typeof BigInt);
