1a6cf2fd54ab884415a3be66b39b96bfda46a4fa
abhay-raj24 <websbase90@gmail.com>
2024-07-19 17:07:10 +0530

    datestime

---
+++ datesin.js
@@ -0,0 +1,29 @@
+// Dates
+
+// const myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toString())
+// console.log(typeof myDate)
+
+// let MyCreatedDate = new Date(2024, 0, 23)
+// let MyCreatedDate = new Date(2024, 0, 23, 5, 30)
+
+// let MyCreatedDate = new Date("2023-01-10")
+// let MyCreatedDate = new Date("12-20-2024")
+// // console.log(MyCreatedDate)
+ // console.log(MyCreatedDate.toLocaleDateString())
+// // console.log(MyCreatedDate.toLocaleTimeString())
+// console.log(MyCreatedDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+
+// // console.log(myTimeStamp.getTime)
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
