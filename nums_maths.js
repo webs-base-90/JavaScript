73b2ecf30655444a05a88306ac1acecbc0d3939a
abhay-raj24 <websbase90@gmail.com>
2024-07-19 16:21:24 +0530

    math and num

---
+++ nums_maths.js
@@ -0,0 +1,22 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))
+
+
+// const hundreds = 1000000000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++ Maths ++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(8.0))
+// console.log(Math.ceil(4.1))
+// console.log(Math.random())
+const min = 10
+const max = 20
+// console.log(Math.floor(Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
